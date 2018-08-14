d8be535f8ddd86d811b92bc2943e0c776563ef88
irwan cigist <cigist@users-MacBook-Pro.local>
2018-08-14 16:09:25 +0700

    ok

--- src/home/home.js
+++ src/home/home.js
@@ -3,14 +3,34 @@ import {Platform, StyleSheet, Text, View} from 'react-native';
 
 
 export default class Home extends Component{
+    constructor(){
+        super();
+        this.updateMyState = this.updateMyState.bind(this);
+    }
     state = {
         myState: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit'
     }
+
+    updateMyState = () => this.setState(
+        {
+            myState:'Dodol Garut'
+        }
+    )
     render() {
         return (
           <View>
-            <Text>{this.state.myState}</Text>
+            <Text style={styles.container} onPress={this.updateMyState}>{this.state.myState}</Text>
           </View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: 'green',
+    }
+  });
+  
